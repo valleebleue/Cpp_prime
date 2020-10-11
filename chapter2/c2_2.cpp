548e24473a324a2f00147da23cf8d6a0ed8d1cf2
valleebleue <yujie_hu@hotmail.fr>
2020-10-11 14:56:51 +0200

    add of c2_2 and c2_3

---
+++ chapter2/c2_2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main()
+{
+    int i;
+    std::cout << "please enter an interger"<<std::endl;
+    std::cin >> i ;
+    std::cout << "The value you entered is "<<i <<"\n";
+    std::cout << " num of inches is " << i*220 << ".\n";
+
+
+}
---
+++ chapter2/c2_3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+
+int function_1()
+{
+    std::cout<<"tree behind mice"<<std::endl;
+    std::cout<<"tree behind mice"<<std::endl;
+}
+
+
+
+int function_2()
+{
+    std::cout<<"see how they run"<<std::endl;
+    std::cout<<"see how they run"<<std::endl;
+}
+
+int main()
+{
+    function_1();
+    function_2();
+
+}
