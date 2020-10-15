00893c0810cd0be0ac7aab68722a8cdb3f555675
valleebleue <yujie_hu@hotmail.fr>
2020-10-15 14:45:57 +0200

    first two in chapter 5

--- chapter5/c5_1.cpp
+++ chapter5/c5_1.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 using namespace std;
-
+//为什么这个生成不了可执行的文件
 int main()
 {
     int num_1;
     int num_2;
-    int i;
-    int j;
+
     cout << "enter a number :" << endl;
     cin >> num_1;
     cout << "enter another number " << endl;
     cin >> num_2;
-    for (i = 0; i = num_1; i ++)
+    for (int i = 0; i = num_1; i ++)
     {
-        for (j = 0; j = num_2; j++)
+        for (int j = 0; j = num_2; j++)
         {
             cout << "now variable i is " << printf("%d",i) << endl;
             cout << "now variable j is " << printf("%d",j) << endl;
