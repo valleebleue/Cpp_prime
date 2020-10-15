862a78ff23591ba749e984c344362975c1c1c2b3
valleebleue <yujie_hu@hotmail.fr>
2020-10-15 14:56:36 +0200

    look at c4_9

--- chapter4/c4_3.cpp
+++ chapter4/c4_3.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 int main(){
-    char last_name;
-    char first_name;
+    char * last_name = new char[10];
+    char * first_name = new char[5];
     char whole_name[80];
-    cout << "Please enter your last name : ";
-    cin >> last_name;
-    cout << "Please enter your first name : ";
-    cin >> first_name;
+    //cout << "Please enter your last name : ";
+    //cin >> last_name;
+    //cout << "Please enter your first name : ";
+    //cin >> first_name;
     strcat(whole_name, last_name);
     strcat(whole_name, first_name);
     cout << whole_name;
